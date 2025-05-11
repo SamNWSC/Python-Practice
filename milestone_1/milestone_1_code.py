5d449463cbc03bc5ac45fe34899a81d2b4e6a286
SamNWSC <norries@wsc.school.nz>
2025-05-11 21:46:31 +1200

    Completed milestone 1

---
+++ milestone_1/milestone_1_code.py
@@ -0,0 +1,7 @@
+#asks the user for pole lap and and their own laptime1
+pole_lap= float(input("What was the pole lap in seconds"))
+your_lap = float(input("What was your lap in seconds"))
+#finds the difference 
+difference = your_lap - pole_lap 
+#prints difference 
+print("{} seconds was the gap to pole".format(difference)) 
