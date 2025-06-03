3243c3303bf44764986d24530d84de5f313fe77c
SamNWSC <159727830+SamNWSC@users.noreply.github.com>
2025-06-03 12:07:18 +1200

    Update milestone_3.py

--- milestone_3/milestone_3.py
+++ milestone_3/milestone_3.py
@@ -2,14 +2,21 @@ import random
 
 score = 0
 questions = [
-    "Who won the 1994 F1 World Championship?", 
-    "Which Driver has the most wins?", 
-    "Which driver has the most Grand Prix starts?"
+    "Who won the 1994 F1 World Championship?""", 
+    "Which Driver has the most wins?""", 
+    "Which driver has the most Grand Prix starts?""",
+    "Who won five consectutive world drivers championships with Ferrari?""",
+    "How many WDC's does Fernando Alonso have?""",
+    "Who won the WDC in 2007""",
 ]
 correct_answers = {
-    "Who won the 1994 F1 World Championship?": "micheal schumacher", 
-    "Which Driver has the most wins?": "lewis hamilton", 
-    "Which driver has the most Grand Prix starts?": "fernando alonso"
+    "Who won the 1994 F1 World Championship?""": "michael schumacher", 
+    "Which Driver has the most wins?""": "lewis hamilton", 
+    "Which driver has the most Grand Prix starts?""": "fernando alonso",
+    "Who won five consectutive world drivers championships with Ferrari?""": "michael schumacher",
+    "How many WDC's does Fernando Alonso have?""": "2",
+    "Who won the WDC in 2007""": "kimi raikkonen"
+
 }
 
 playing = True
@@ -20,10 +27,13 @@ while playing:
     for question in questions:
         user_answer = input("What's the answer to '{}'? ".format(question)).strip().lower()
         if user_answer == correct_answers.get(question, ""):
-            print("Good Job")
+            print("")
+            print("Good Job, you got it right.\n")
             score += 10
         else:
-            print("Incorrect answer.")
+            print("")
+            print("Incorrect answer, maybe next time.\n")
+            
     
     print("Score: {}".format(score))
     playing_status = input("Are you still playing? (Y/N): ").strip().lower()
